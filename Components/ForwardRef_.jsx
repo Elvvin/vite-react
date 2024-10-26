eaab5726a0907246158a53dc45779f79ad5d8f05
Elvvin <elvin.babazade.1010.eb.eb@gmail.com>
2024-10-26 16:40:36 +0400

    updates

--- Components/ForwardRef_.jsx
+++
@@ -1,8 +0,0 @@
-import { forwardRef } from "react";
-
-// eslint-disable-next-line react/display-name
-    const Input = forwardRef((props, ref) => {
-        return <input type='text' ref={ref} {...props}/>
-    })
-
-export default Input
